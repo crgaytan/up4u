c598a27dd702b3647b0b7f8e00fbfe1a1c48fdd5
crgaytan <crgaytan26@gmail.com>
2022-11-13 09:57:12 -0600

    Update and rename index.html to index.php

--- up4u.up.edu.mx/index.php
+++ up4u.up.edu.mx/index.php
@@ -4,7 +4,7 @@
 <!-- Mirrored from up4u.up.edu.mx/ by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 13 Nov 2022 03:22:10 GMT -->
 <!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
 <HEAD>
-<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8"><META HTTP-EQUIV="Refresh" CONTENT="0; URL=user/auth/login.html"><TITLE>Page has moved</TITLE>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8"><META HTTP-EQUIV="Refresh" CONTENT="0; URL=user/auth/login.php"><TITLE>Page has moved</TITLE>
 </HEAD>
 <BODY>
 <A HREF="user/auth/login.html"><h3>Click here...</h3></A>
